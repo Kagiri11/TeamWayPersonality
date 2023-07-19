0a54f8253111197756da6c6619e526a7852be557
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2023-07-19 22:18:57 +0300

    Adds repo methods

--- data/repository/src/main/java/com/maina/repository/datasources/AppRepositoryImpl.kt
+++ data/repository/src/main/java/com/maina/repository/datasources/AppRepositoryImpl.kt
@@ -1,7 +1,17 @@
 package com.maina.repository.datasources
 
+import com.maina.domain.models.Question
+import com.maina.domain.models.Trait
 import com.maina.domain.repository.AppRepository
 import com.maina.network.api.PersonalityRemoteSource
 
 class AppRepositoryImpl(private val remoteSource: PersonalityRemoteSource): AppRepository {
+
+    override suspend fun getQuestion(questionNumber: Int): Result<Question> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun submitAnswer(answer: String): Result<Trait> {
+        TODO("Not yet implemented")
+    }
 }
--- domain/src/main/java/com/maina/domain/repository/AppRepository.kt
+++ domain/src/main/java/com/maina/domain/repository/AppRepository.kt
@@ -1,4 +1,12 @@
 package com.maina.domain.repository
 
+import com.maina.domain.models.Question
+import com.maina.domain.models.Trait
+
 interface AppRepository {
+
+    suspend fun getQuestion(questionNumber: Int): Result<Question>
+
+    suspend fun submitAnswer(answer: String): Result<Trait>
+
 }
